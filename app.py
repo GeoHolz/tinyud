9fecffaec71444814e9adf32ee96263021c83c18
GeoHolz <nourssh@gmail.com>
2023-10-06 15:01:09 +0200

    Starting flask

---
+++ app.py
@@ -0,0 +1,83 @@
+import sqlite3
+import time
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
+from dotenv import load_dotenv
+load_dotenv()
+
+def get_db_connection():
+    conn = sqlite3.connect('tinyud.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM tinyud WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+@app.template_filter('ctime')
+def timectime(s):
+    return time.ctime(s) # datetime.datetime.fromtimestamp(s)
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM tinyud').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        ipaddr = request.form['ipaddr']
+        nbcheck = request.form['nbcheck']
+
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO tinyud (nom ,addr ,state ,attempt_fail ,lasttime_down , check_attempt ) VALUES (?,?,"Up",0,0,?)',(title, ipaddr,nbcheck))
+            conn.commit()
+            conn.close()
+            flash('"{}" was successfully added!'.format(title))
+            return redirect(url_for('index'))
+    
+    return render_template('create.html')
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['nom']
+        ipaddr = request.form['addr']
+        nbcheck = request.form['check_attempt']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE tinyud SET nom = ?, addr = ?, check_attempt = ? WHERE id = ?',(title, ipaddr, nbcheck,id))
+            conn.commit()
+            conn.close()
+            flash('"{}" was successfully edited!'.format(title))
+            return redirect(url_for('index'))
+    
+    return render_template('edit.html', post=post)
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM tinyud WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['nom']))
+    return redirect(url_for('index'))
--- tinyud.py
+++ tinyud.py
@@ -1,9 +1,9 @@
 import argparse
-from tinydb import TinyDB, Query
 from ping3 import ping
 from datetime import datetime
 import requests #pip install requests
 from datetime import datetime
+import sqlite3
 
 # The argparse module makeis it easy to write user-friendly command-line interfaces
 parser = argparse.ArgumentParser()
@@ -11,23 +11,31 @@ parser.add_argument("-a","--add", help="Add a service", action="store_true")
 parser.add_argument("-d","--delete", help="Delete a service", action="store_true")
 parser.add_argument("--name", help="Name of service")
 parser.add_argument("--address", help="Address IP")
-parser.add_argument("--check", help="Number of times the service is detected down before being notified")
+parser.add_argument("--checktimes", help="Number of times the service is detected down before being notified")
 parser.add_argument("--list", help="List", action="store_true")
 parser.add_argument("--test", help="Test Gotify notification", action="store_true")
+parser.add_argument("--gotify", help="Add Gotify URL")
 args = parser.parse_args()
 
-# Initialize TinyDB
-db = TinyDB('tinydb.json')
-Element = Query()
 
-# Variable
-gotify_url=db.search(Element.nom =="GotifyURL")
+def get_db_connection():
+    # SQLite3
+    database = sqlite3.connect('tinyud.db')
+    database.execute('CREATE TABLE IF NOT EXISTS tinyud (	id INTEGER PRIMARY KEY ,	nom VARCHAR(100),	addr VARCHAR(100),	state VARCHAR(10),	attempt_fail INT(5),	lasttime_down FLOAT(100),check_attempt INT(5));')
+    database.execute('CREATE TABLE IF NOT EXISTS tinyud_config (id INTEGER PRIMARY KEY ,	name VARCHAR(100),	config VARCHAR(100))')
+    
+    return database
+
 
 # Function to alert via Gotify
 def alert_gotify(name,state,lasttime_down):
+    database=get_db_connection()
+    cursor=database.cursor()
+    cursor.execute("SELECT config FROM tinyud_config WHERE name='GotifyURL'")
+    gotify_url=cursor.fetchone()[0]
     date_time = datetime.fromtimestamp(lasttime_down)
     if state == "Down":
-        requests.post(gotify_url[0]["addr"], json={
+        requests.post(gotify_url, json={
         "message": "The " + name + " service is "+state + " since : " + date_time.strftime("%d-%m-%Y, %H:%M:%S"),
         "priority": 2,
         "title": "Service : "+name+" : "+state
@@ -36,11 +44,12 @@ def alert_gotify(name,state,lasttime_down):
         now = datetime.now() # current date and time
         then =datetime.fromtimestamp(lasttime_down)
         duration = now - then
-        requests.post(gotify_url[0]["addr"], json={
+        requests.post(gotify_url, json={
         "message": "The " + name + " service is "+state + ". Duration : " + str(duration.total_seconds() / 60) + " minutes",
         "priority": 2,
         "title": "Service : "+name+" : "+state
-        })        
+        })   
+    database.close()     
 
 # Function to check ping3
 def check_ping(addresses):
@@ -51,57 +60,92 @@ def check_ping(addresses):
         return "Up"
 # Check principal function
 def check():
-    for data in db:
-        if data["nom"] == "GotifyURL":
-            continue
-        oldstate = data["state"]
-        newstate = check_ping(data["addr"])
+    database=get_db_connection()
+    cursor=database.cursor()
+    cursor.execute("SELECT * FROM tinyud")
+    result=cursor.fetchall()
+    for data in result:
+        print(data)
+        oldstate = data[3]
+        newstate = check_ping(data[2])
         # print(data["nom"])
         if oldstate == "Down" and newstate == "Up" :
-            db.update({'state':'Up'},Element.nom == data["nom"])
-            if int(data["attempt_fail"]) >= int(data["check_attempt"]) :
+            database.execute("""UPDATE tinyud SET state = 'Up' WHERE nom = ?""",(data[1],))
+            #db.update({'state':'Up'},Element.nom == data["nom"])
+            if int(data[4]) >= int(data[6]) :
                 print("Notifier de nouveau Up")
-                alert_gotify(data["nom"],"Up",data["lasttime_down"])
-            db.update({'attempt_fail':'0'},Element.nom == data["nom"])
+                alert_gotify(data[1],"Up",data[5])
+            #db.update({'attempt_fail':'0'},Element.nom == data["nom"])
+            database.execute("""UPDATE tinyud SET attempt_fail = 0 WHERE nom = ?""",(data[1],))
         elif oldstate == "Up" and newstate == "Down":
-            db.update({'state':'Down'},Element.nom == data["nom"])
-            db.update({'attempt_fail':'1'},Element.nom == data["nom"])
+            #db.update({'state':'Down'},Element.nom == data["nom"])
+            database.execute("""UPDATE tinyud SET state = 'Down' WHERE nom = ?""",(data[1],))
+            #db.update({'attempt_fail':'1'},Element.nom == data["nom"])
+            database.execute("""UPDATE tinyud SET attempt_fail = 1 WHERE nom = ?""",(data[1],))
             now = datetime.now() # current date and time
-            db.update({'lasttime_down':now.timestamp()},Element.nom == data["nom"])
+            #db.update({'lasttime_down':now.timestamp()},Element.nom == data["nom"])
+            database.execute("""UPDATE tinyud SET lasttime_down = ? WHERE nom = ?""",(now.timestamp(),data[1]))
         elif oldstate == "Down" and newstate == "Down":
-            nb_fail=int(data["attempt_fail"])
+            nb_fail=int(data[4])
             nb_fail +=1
-            db.update({'attempt_fail':nb_fail},Element.nom == data["nom"])
-            if nb_fail == int(data["check_attempt"]):
+            #db.update({'attempt_fail':nb_fail},Element.nom == data["nom"])
+            database.execute("""UPDATE tinyud SET attempt_fail = ? WHERE nom = ?""",(nb_fail,data[1]))
+            if nb_fail == int(data[6]):
                 print("Notifier Down")
-                alert_gotify(data["nom"],"Down",data["lasttime_down"])
+                alert_gotify(data[1],"Down",data[5])
+    database.commit()
+    database.close()
 
 def main():
 
-    if args.add and (args.name is None or args.address is None or args.check_attempt is None):
+    if args.add and (args.name is None or args.address is None or args.checktimes is None):
         parser.error('--add requires --name, --address and --check')
     if args.delete and (args.name is None ):
         parser.error('--delete requires --name')
 
     #For Debug     
-    print("args=%s" % args)
+    #print("args=%s" % args)
 
 
     if args.list:
-        for data in db:
-            print(data)
+        #for data in db:
+            #print(data)
+        database=get_db_connection()
+        for row in database.execute('SELECT * FROM tinyud'):
+            print(row)
+        database.close()
 
     if args.add:
-        db.insert({'nom': args.name,'addr':args.address,'state':'Up','attempt_fail':'0','lasttime_down':'0','check_attempt':args.check})
-
+       # db.insert({'nom': args.name,'addr':args.address,'state':'Up','attempt_fail':'0','lasttime_down':'0','check_attempt':args.check})
+        database=get_db_connection()
+        database.execute("""
+                         INSERT INTO tinyud (nom ,addr ,state ,attempt_fail ,lasttime_down , check_attempt )
+                         VALUES (?,?,"Up",0,0,?);""",
+                         (args.name,args.address,args.checktimes))
+        database.commit()
+        database.close()
     if args.delete:
-        db.remove(Element.nom == args.name)
-
+        database=get_db_connection()
+        #db.remove(Element.nom == args.name)
+        database.execute("""DELETE FROM tinyud WHERE nom=?""",(args.name,))
+        database.commit()
+        database.close()
     if args.test:
-        alert_gotify("Test","Test")
-
+        database=get_db_connection()
+        now = datetime.now()
+        alert_gotify("Test","Up",now.timestamp())
+        database.close()
+    if args.gotify:
+        database=get_db_connection()
+        database.execute("""
+                         INSERT INTO tinyud_config (name ,config)
+                         VALUES ('GotifyURL',?);""",
+                         (args.gotify,))
+        database.commit()
+        database.close()
     if not any(vars(args).values()):
         check()
 
+    
 if __name__ == "__main__":
     main()
